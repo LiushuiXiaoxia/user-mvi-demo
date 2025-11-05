2ec3e31034d3bd46343a4b06c309ca4519f1fc88
leon <xiaqiulei@126.com>
2025-11-06 01:32:13 +0800

    koin

--- composeApp/src/commonMain/kotlin/com/example/user_mvi_demo/Platform.kt
+++ composeApp/src/commonMain/kotlin/com/example/user_mvi_demo/Platform.kt
@@ -4,7 +4,6 @@ import com.example.user_mvi_demo.user.UserRepository
 import com.example.user_mvi_demo.user.UserViewModel
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
-import org.koin.compose.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
